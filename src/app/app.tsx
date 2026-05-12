fda627a976cb13089d10d5a97c4611bb7c18555a
Ivan Smirnov <chaekplayyt@gmail.com>
2026-05-12 19:53:53 +0300

    styles refactoring (everything's broken)

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,33 +1,33 @@
-import './styles/app.css'
-import {About} from "@/pages/about_page/about";
-import {ErrorBoundary, LocationProvider, Route, Router} from 'preact-iso';
-import {Posts} from "@/pages/posts";
-import {PostDetails} from "@/pages/post_view_page";
-import { NotFound } from './routes';
-import { Auth } from '@/pages/auth_page/ui/auth_container';
-import { BaseLayout } from './layouts/base_layout';
+import "./styles/global.css";
+import { About } from "@/pages/about_page/about";
+import { ErrorBoundary, LocationProvider, Route, Router } from "preact-iso";
+import { Posts } from "@/pages/posts";
+import { PostDetails } from "@/pages/post_view_page";
+import { NotFound } from "./routes";
+import { Auth } from "@/pages/auth_page/ui/auth_container";
+import { BaseLayout } from "./layouts/base_layout";
 
 function wrapWithLayout(Component: any) {
-    return (props: any) => (
-        <BaseLayout>
-            <Component {...props}/>
-        </BaseLayout>
-    )
+  return (props: any) => (
+    <BaseLayout>
+      <Component {...props} />
+    </BaseLayout>
+  );
 }
 
 export function App() {
-    return (
-        <LocationProvider>
-            <ErrorBoundary>
-                <Router>
-                    <Route path="/" component={wrapWithLayout(Posts)}></Route>
-                    {/*<Route path="/profile/:id" component={}></Route>*/}
-                    <Route path="/posts/:id" component={wrapWithLayout(PostDetails)} />
-                    <Route path="/about" component={wrapWithLayout(About)}></Route>
-                    <Route component={NotFound} default />
-                    <Route path='/auth' component={wrapWithLayout(Auth)}/>
-                </Router>
-            </ErrorBoundary>
-        </LocationProvider>
+  return (
+    <LocationProvider>
+      <ErrorBoundary>
+        <Router>
+          <Route path="/" component={wrapWithLayout(Posts)}></Route>
+          {/*<Route path="/profile/:id" component={}></Route>*/}
+          <Route path="/posts/:id" component={wrapWithLayout(PostDetails)} />
+          <Route path="/about" component={wrapWithLayout(About)}></Route>
+          <Route component={NotFound} default />
+          <Route path="/auth" component={wrapWithLayout(Auth)} />
+        </Router>
+      </ErrorBoundary>
+    </LocationProvider>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
-import { render } from 'preact'
-import './app/styles/index.css';
-import { App } from './app/app.tsx'
+import { render } from "preact";
+import { App } from "./app/app.tsx";
 
-render(<App />, document.getElementById('app')!)
+render(<App />, document.getElementById("app")!);
